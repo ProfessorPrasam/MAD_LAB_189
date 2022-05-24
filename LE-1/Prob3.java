f73c86a9bfc6f5244a3881efdc4a372e899c9ebf
ProfessorPrasam <201B189@juetguna.in>
2022-05-24 12:39:19 +0530

    upload java files

--- LE-1/Prob3.java
+++ LE-1/Prob3.java
@@ -1,6 +1,6 @@
 /*
 Write a program in Java to print the table of a number received through command
-line argument, e.g.
+line argument, e.g:-
 
 If user gives 4 through command line argument, the program should print:
     4 x 1= 4
