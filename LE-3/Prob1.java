f840b906bb9392ec74fefee9c60db955ab13ccc2
pj1807 <prasamjain294@gmail.com>
2022-05-27 11:32:05 +0530

    add files

---
+++ LE-3/Prob1.java
@@ -0,0 +1,14 @@
+/*
+1. Make your first inheritance based design
+There were Pandavs and Kauravs. Arjun and Bheem were Pandavs. Duryodhan was a
+Kaurav. Pandavs were characterized by their skills of fighting (fight ( )), obedience (obey
+( )), and kindness (kind ( )). Though Bheem was little less kind but equally obedient as
+Arjun. Kauravs were notorious for disobeying and cruelty. But Kauravs were fighters.
+Kauravs were 100 in numbers, but one of them ‘Vikarn’ was a noble man- a good
+fighter, kind and obedient. If you dive a little more in the history, you will come to know
+that Pandavs and Kauravs were actually Bharatvanshi. And all bharatvanshis had been
+fighters.
+You are required to create a design first on paper and then write implementation on
+your machine. Make abstract classes and concrete classes. Decide which methods
+should be made abstract or non-abstract.
+*/
