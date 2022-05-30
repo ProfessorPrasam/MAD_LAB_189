436c4c2c5deb4f644990f33090c468b5901b2a05
ProfessorPrasam <104970432+ProfessorPrasam@users.noreply.github.com>
2022-05-30 11:11:53 +0530

    Update Prob2.java

--- LE-1/Prob2.java
+++ LE-1/Prob2.java
@@ -6,7 +6,7 @@
   * * * *            *
 Hint. Use for loop in java, the syntax is same as that in C++.   */
 
-public class LE1_Prob2
+public class LE1_Prob2a
 {
 	public static void main(String[] args){
 	int rows = Integer.parseInt(args[0]);
@@ -21,4 +21,23 @@ public class LE1_Prob2
 
 	}
      }
+}
+
+
+//Pattern -2
+
+public class LE1_Prob2b
+{
+	public static void main(String[] args) {
+	   int rows = Integer.parseInt(args[0]);
+	   for (int i = 1 ; i <= rows ; i++){
+	       for ( int j = 1 ; j <= i ; j++)
+	        System.out.print(" ");
+	        
+	        for ( int k = rows ; k >=i ; k-- )
+	        System.out.print(" *");
+	        
+	        System.out.println();
+	   }
+	}
 }
