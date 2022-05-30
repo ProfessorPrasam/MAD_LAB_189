9e61d90a3577c882a15d5ef770018c9116f4c2e7
ProfessorPrasam <104970432+ProfessorPrasam@users.noreply.github.com>
2022-05-30 11:10:58 +0530

    Create Prob3.java

---
+++ LE-1/Prob3.java
@@ -0,0 +1,11 @@
+/*
+Write a program in Java to print the table of a number received through command
+line argument, e.g.
+
+If user gives 4 through command line argument, the program should print:
+    4 x 1= 4
+    4 x 2= 8
+    ............
+    4x10= 40
+*/
+
