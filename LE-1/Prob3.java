8be1393a60657642d4d4de32214cd276a29fb574
ProfessorPrasam <104970432+ProfessorPrasam@users.noreply.github.com>
2022-05-30 11:12:24 +0530

    Update Prob3.java

--- LE-1/Prob3.java
+++ LE-1/Prob3.java
@@ -9,3 +9,13 @@ If user gives 4 through command line argument, the program should print:
     4x10= 40
 */
 
+public class LE1_Prob4
+{
+	public static void main(String[] args){
+	    int  x = 4;
+	    for ( int i = 1 ; i <=10 ; i++){
+		System.out.println(x +" * " + i + " = " + (x*i));  	
+
+	}
+     }
+}
